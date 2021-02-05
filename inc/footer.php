8a7388f904c07bc09a004e4c41fa24269a14eec3
jnknnthpadul <201880484@psu.palawan.edu.ph>
2021-02-05 22:18:27 +0800

    first commit

---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="mastfoot mt-auto" style="text-align: center;">
+    <div class="inner">
+      <p>Created by <a href="https://github.com/jnknnthpadul">Jan Kenneth L. Padul </a>  <a href="https://github.com/Charmaender">Charm Mae Garcellano </a> <a href="https://github.com/Crence18">Chris Lawrence Consebido  </a> for BSCS 3B2</p>
+    </div>
+</footer>
+  </body>
+</html>
